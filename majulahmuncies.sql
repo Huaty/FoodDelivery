43eefef9352fc43765266c4299a127167ce28450
Huaty <siaosiaoboy@gmail.com>
2023-09-04 10:09:58 +0800

    Upload Db Connection

--- majulahmuncies.sql
+++ majulahmuncies.sql
@@ -1,5 +1,5 @@
 
-CREATE DATABASE MajulahMunchies;
+
 USE MajulahMunchies;
 
 CREATE TABLE users(
@@ -8,25 +8,26 @@ CREATE TABLE users(
     lastname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     pwd VARCHAR(255) NOT NULL,
-    home_address VARCHAR(255) NOT NULL
-
+    homeaddress VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE menu (
+CREATE TABLE menus (
     item_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     foodname VARCHAR(255) NOT NULL,
     food_description VARCHAR(255) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
+    price DECIMAL(10,2) NOT NULL
 
-)
+);
 
 
-CREATE TABLE order(
+CREATE TABLE orders (
     order_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    FOREIGN KEY (user_id) REFERENCES user(user_id),
-    FOREIGN KEY (item_id) REFERENCES menu(item_id),
+    user_id INT NOT NULL,
+    item_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (item_id) REFERENCES menus(item_id),
     order_date DATE NOT NULL,
-    quantity INT NOT NULL,
+    quantity INT NOT NULL
 
-)
+);
 
