1241111ee37f6335a53a356bcfea5121e3e9bee6
Huaty <siaosiaoboy@gmail.com>
2023-09-04 21:54:57 +0800

    Changing Db location

---
+++ html/signup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+
+  <?php
+  
+  include("../asset/db_connection.php");
+  $conn = Opencon();
+  session_start();
+
+  $_SESSION['username'] = "test1";
+
+  ?>
+  <body>
+    <!-- NAV Component -->
+    <header class= "header">
+      
+    </header>
+    <div>
+      <a href = "login.php">Login</a>
+    </div>
+    <!-- <form method="POST" action="signup.php">
+      <input type="text" name="firstname" placeholder="First Name" /><br />
+      <input type="text" name="lastname" placeholder=" Last Name" /><br />
+      <input type="text" name="email" placeholder="Email" /><br />
+      <input type="password" name="password" placeholder="Password" /><br />
+      <input type="text" name="homeaddress" placeholder="Home Address " /><br />
+      <input type="Submit" value="Submit" /><br />
+    </form> -->
+  </body>
+</html>
