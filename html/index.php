4077031f1a7af76583d5725e64aa7c95306df24f
Huaty <siaosiaoboy@gmail.com>
2023-09-09 02:25:21 +0800

    Update file

--- html/index.php
+++ html/index.php
@@ -1,17 +1,89 @@
+<?php
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css" />
-    <title>Document</title>
-  </head>
-  <nav>
-    <div class="logo" >
-      <h4>Logo</h4>
 
-  </nav>
-  <body>
-    <a href="signup.php">Sign Up</a>
-  </body>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="https://fonts.google.com/specimen/Just+Another+Hand">
+  <title>Majulah Munchies</title>
+</head>
+
+<body>
+  <header>
+    <!-- Header -->
+    <div class="nav-container">
+      <!-- Logo -->
+      <div class="logo-placement">
+        <a href="index.php"><object data="../asset/image/Logo.svg" Alt="Logo" class="logo"></object></a>
+      </div>
+      <!-- End Logo -->
+      <!-- Navigation Bar -->
+      <div class="nav-flex-right nav-flex-grow">
+        <span class="nav-content"><a href="index.php"> Home</a></span>
+        <span class="nav-content">Menu</span>
+        <span class="nav-content"><a href="about.php">About Us</a></span>
+        <span class="nav-content"><a href="logins.php">Log In </a></span>
+      </div>
+    </div>
+  </header>
+  <!-- End Navigation Bar -->
+  <!-- End Header -->
+
+  <!-- Main Body -->
+  <div class="main-container">
+    <!-- Top Content-->
+    <div class="top">
+      <!-- Top Left Content -->
+      <div class="left-content">
+        <div>
+          <h1 id="header">From Chef's Pan to your Plate, <br><span id="red-heading">Swift</span> and <span id="red-heading">Great!</span></h1>
+        </div>
+        <div>
+          <p id="body">Food Assures Fresh</p>
+        </div>
+      </div>
+      <!--- End Top Left Content -->
+      <!-- Top Right Content -->
+      <div class="right-content">
+        <img src="../asset/image/manridingbicycle.jpg" alt="manridingbicycle" class="img">
+      </div>
+      <!-- End Top Right Content -->
+    </div>
+    <!-- End Top Content -->
+    <!-- Bottom Content -->
+    <div class="bottom">
+      <p>Hello</p>
+    </div>
+    <!-- End Bottom Content -->
+  </div>
+  <!-- End Main Body -->
+  <!--Footer -->
+  <footer>
+    <!-- Footer Top -->
+    <div class="top">
+      <div class="smallLogo">
+        <object data="../asset/image/smallLogo.svg" Alt="smallLogo" id="img"></object>
+        <p id="word">Munchies Together</p>
+      </div>
+      <div class="contact">
+        <p>Call us</p>
+      </div>
+      <div class="statement">
+        <p>Privacy Policy</p>
+        <p>Terms & Conditions</p>
+      </div>
+
+    </div>
+    <!-- End Footer Top -->
+    <div class="bottom">
+      <p>&copy 2023 Majulah Munchies. All rights reserved.</p>
+
+    </div>
+  </footer>
+</body>
+
 </html>
