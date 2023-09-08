a50d1e4e9d403e601d54d0616d9138aab600c47c
Huaty <siaosiaoboy@gmail.com>
2023-09-09 02:21:48 +0800

    Update Content

--- html/login.php
+++ html/login.php
@@ -1,18 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <?php
 
-  session_start();
-  echo $_SESSION['username'];
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+<?php
 
-  ?>
-  
-  <body>
-    <form method="POST "></form>
-  </body>
+session_start();
+echo $_SESSION['username'];
+
+?>
+
+<body>
+  <!-- Header Compenent Consist of Logo and Nav Bar !-->
+  <header>
+    <div class="logo">
+
+      <img src="/asset/image/Logo.png" alot="Logo" />
+    </div>
+
+    <nav>
+
+    </nav>
+
+  </header>
+
+</body>
+
 </html>
