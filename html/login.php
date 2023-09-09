bf5c00374aab325fbdd4f5b4235f52f1a601b8b0
Huaty <siaosiaoboy@gmail.com>
2023-09-09 14:51:27 +0800

    Update scriptjs and Index.php

--- html/login.php
+++ html/login.php
@@ -4,20 +4,13 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css" />
   <title>Majulah Munchies</title>
+
 </head>
 
-
 <body>
-  <header>
-    <div class="back-arrow">
-      <a href="index.php">
-        <object data="../asset/image/Backarrow.svg" Alt="Backarrow" class="backarrow"></object>
-      </a>
-
-    </div>
-  </header>
-  <form method="POST "></form>
 </body>
+<script src="../asset/js/script.js"></script>
 
 </html>
