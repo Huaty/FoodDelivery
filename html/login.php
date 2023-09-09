934152595d0b1cc19e4de3fbbbe57837c9456b00
Huaty <siaosiaoboy@gmail.com>
2023-09-09 22:37:30 +0800

    update login.php

--- html/login.php
+++ html/login.php
@@ -5,12 +5,33 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="https://fonts.google.com/specimen/Just+Another+Hand">
   <title>Majulah Munchies</title>
-
 </head>
 
 <body>
+  <header>
+    <object data="../asset/image/Backarrow.svg" Alt="Back" class="back"></object>
+  </header>
+  <div class="main-container-login-register">
+    <div class="wrapper-login">
+      <form method="GET" action="" class="login-input">
+        <label for="email">Email</label>
+        <input type="email" name="username" id="email" class="email" />
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" class="password" />
+        <input type="submit" value="login" class="login" />
+      </form>
+      <div class="link-signup">
+        <p>If you don't have an account, <a href="signup.php">Sign up</a> here!</p>
+      </div>
+      <div class="smallLogo">
+        <object data="../asset/image/smallLogo.svg" Alt="smallLogo" id="img"></object>
+        <p id="word">Munchies Together</p>
+      </div>
+    </div>
+  </div>
 </body>
-<script src="../asset/js/script.js"></script>
+
 
 </html>
