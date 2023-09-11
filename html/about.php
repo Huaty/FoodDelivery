ddf4709fe8debde3ef2aaaee6a860319a905d74c
BingweixD <bingwei98@hotmail.com>
2023-09-12 00:03:30 +0800

    test

--- html/about.php
+++ html/about.php
@@ -3,9 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="https://fonts.google.com/specimen/Just+Another+Hand">
+  <title>Majulah Munchies</title>
 </head>
 <body>
-    
+    <header>
+        <!-- Header -->
+        <div class="nav-container">
+        <!-- Logo -->
+            <div class="logo-placement">
+                <a href="index.php"><object data="../asset/image/Logo.svg" Alt="Logo" class="logo"></object></a>
+            </div>
+        <!-- End Logo -->
+        <!-- Navigation Bar -->
+            <ul class="nav-flex-right nav-flex-grow">
+                <li class="nav-content"><a href="index.php"> Home</a></li>
+                <li class="nav-content">Menu</li>
+                <li class="nav-content about-us"><a href="about.php">About Us</a></li>
+                <li class="nav-content login"><a href="login.php">Log In </a></li>
+        </div>
+    </header>
+
+    <div class = "aboutus-content">
+        <div class = "top">
+            <h1>
+                About Us
+            </h1>
+                <p>
+                Welcome to MajulahMunchies, your passport to a world of culinary delights delivered right to your doorstep! We're more than just a food delivery service; we're your partners in savoring the finest flavors from around the globe.
+                </p>    
+        </div>
+    </div>
+
 </body>
 </html>
