05eb11dc4b29d2735eaf1e33772340ea993b13cd
Huaty <siaosiaoboy@gmail.com>
2023-10-22 01:10:18 +0800

    update

--- other_project/index.php
+++ other_project/index.php
@@ -1,3 +1,10 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $selectedSeats = array_keys($_POST);  // Get the keys of the $_POST array, which are the names of the checked checkboxes
+    echo "Selected seats: " . implode(", ", $selectedSeats);  // Print the selected seats as a comma-separated string
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,14 +25,18 @@
         <div id="movie-screen">
             <div id="screen">Screen</div>
             <div id="movie">
-                <div id="movieseats">
-                </div>
+                <form method="POST" action="" id="selectedCheckBoxForm">
+                    <div id="movieseats"></div>
+                </form>
             </div>
         </div>
         <div id="checkout-container">
             Checkout
-            <div id="checkout-seats"></div>
+            <div id="print-seats">
 
+                <div id="checkout-seats"></div>
+            </div>
+            <button type="submit" id="checkout-button">Checkout</button>
         </div>
         <!-- <button type="submit" id="checkout-button">Checkout</button> -->
     </div>
