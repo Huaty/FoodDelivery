95a392395324b3b313419eadd163f7b3875ac8de
Huaty <siaosiaoboy@gmail.com>
2023-11-04 20:49:17 +0800

    Update

--- html/changeEmail.php
+++ html/changeEmail.php
@@ -24,10 +24,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $newEmail = $_POST['newEmail'];
     $confirmNewEmail = $_POST['confirmNewEmail'];
     $emailRegex = '/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+$/';
-    
+
     if (!preg_match($emailRegex, $newEmail)) {
         $catchErrorMessage = "Error: Invalid email format for the new email address.";
-    } else if (strtolower($currentEmailInput) !== strtolower($currentEmail)){
+    } else if (strtolower($currentEmailInput) !== strtolower($currentEmail)) {
         $catchErrorMessage = "Error: The provided current email does not match your account.";
     } else if ($newEmail !== $confirmNewEmail) {
         $catchErrorMessage = "Error: New email and confirm email do not match.";
@@ -47,17 +47,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $updateStmt->bindParam(':newEmail', $newEmail);
                 $updateStmt->bindParam(':UserID', $userId);
                 $updateStmt->execute();
-                $Successful =" Email updated successfully.";
+                $Successful = " Email updated successfully.";
                 $successful = true; // Set the success flag
-                
+
                 header("Location: emailChange.php");
                 exit(); // Ensure the script stops after redirection
             } catch (PDOException $e) {
                 $catchErrorMessage = "Database Error: " . $e->getMessage();
             }
-
-
-
         }
     }
 }
@@ -70,8 +67,6 @@ if (!empty($catchErrorMessage)) {
 ?>
 
 
-?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -80,25 +75,28 @@ if (!empty($catchErrorMessage)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" />
-    <title>Change Email</title>
-    <style>
-        .error-message {
-            color: red;
-        }
-    </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Just+Another+Hand&family=Koulen&family=Lalezar&family=Mitr:wght@200&display=swap" rel="stylesheet">
+    <title>Menu</title>
 </head>
 
+
 <body>
+
     <!-- Header -->
     <div class="nav-container-menu">
         <!-- Logo -->
-        <a href="menu.php" class="button-style">
-            <div class="logo-placement">
-                <img src="../asset/image/Logo.png" class="logo">
-            </div>
-        </a>
+        <div id="logo">
+            <a href="menu.php" class="button-style">
+                <div class="logo-placement">
+                    <img src="../asset/image/Logo.png" class="logo">
+                </div>
+            </a>
+        </div>
         <div class="profile-dropdown">
-            <button class="dropbtn">Welcome, <?php echo $_SESSION["user_firstname"] ?>
+            <button class="dropbtn">
+                <div id="welcome">Welcome, <strong><?php echo $_SESSION["user_firstname"] ?></div></strong>
                 <img src="../asset/image/bingwei.jpeg" alt="Profile Picture" class="profile-pic">
             </button>
             <div class="dropdown-content">
@@ -108,22 +106,86 @@ if (!empty($catchErrorMessage)) {
             </div>
         </div>
     </div>
-    <div class="profile-container">
-        <form method="POST" action="">
-        <?php echo "<div>Current Email: " . $currentEmail . "</div>" ?>
-            <label for="currentEmail">Current Email:</label><br>
-            <input type="email" id="currentEmail" name="currentEmail" required><br><br>
-            <!-- <span id="currentEmailError" class="error-message"></span> -->
-            <label for="newEmail">New Email:</label><br>
-            <input type="email" id="newEmail" name="newEmail" required><br><br>
-            <!-- <span id="newEmailError" class="error-message"></span><br> -->
-            <label for="confirmNewEmail">Confirm New Email:</label><br>
-            <input type="email" id="confirmNewEmail" name="confirmNewEmail" required><br>
-            <!-- <br><span id="confirmNewEmailError" class="error-message"></span><br> -->
-            <br><input type="submit" value="Submit" name="Change Email">
-            <br><?php echo $catchErrorMessage ?>
-        </form>
+
     </div>
+
+
+
+    <div class="profile-container">
+        <nav>
+
+
+            <div class="profile-menu">
+                <a class="menu-item " href="profile.php">Profile</a>
+                <a class="menu-item active" href="changeEmail.php">Change Email</a>
+                <a class="menu-item" href="changePassword.php">Change Password</a>
+                <a class="menu-item " href="changeAddress.php">Change Address</a>
+            </div>
+
+        </nav>
+        <div class="profile-content-container">
+            <header class='title'><span>Change Email</span></header>
+            <div class="profile-info">
+                <form class="profile-form" method="POST" action="">
+                    <div class="input-group">
+                        <?php echo "<div>Current Email: " . $currentEmail . "</div>" ?>
+                    </div>
+                    <div class="input-group">
+                        <label for="currentEmail">Current Email:</label><br>
+                        <input type="email" id="currentEmail" name="currentEmail" required><br><br>
+                        <!-- <span id="currentEmailError" class="error-message"></span> -->
+                    </div>
+                    <div class="input-group">
+                        <label for="newEmail">New Email:</label><br>
+                        <input type="email" id="newEmail" name="newEmail" required><br><br>
+                    </div>
+                    <!-- <span id="newEmailError" class="error-message"></span><br> -->
+                    <div class="input-group">
+                        <label for="confirmNewEmail">Confirm New Email:</label><br>
+                        <input type="email" id="confirmNewEmail" name="confirmNewEmail" required><br>
+                    </div>
+                    <!-- <br><span id="confirmNewEmailError" class="error-message"></span><br> -->
+                    <div class="button-container">
+                        <br><input class="button" type="submit" value="Submit" name="Change Email">
+                    </div>
+                    <br><?php echo $catchErrorMessage ?>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+    <footer>
+        <!-- Footer Top -->
+        <div class="top">
+            <div class="smallLogo">
+                <object data="../asset/image/smallLogo.svg" Alt="smallLogo" id="img"></object>
+                <p id="word">Munchies Together</p>
+            </div>
+            <div class="contact">
+                <p>Contact Us </p>
+                <div class="small-icon">
+                    <img src="../asset/image/instagram.png" alt="instagram" class="img">
+                    <img src="../asset/image/twitter.png" alt="twitter" class="img">
+                </div>
+            </div>
+            <div class="statement">
+                <p>Privacy Policy</p>
+                <p>Terms & Conditions</p>
+            </div>
+
+        </div>
+        <!-- End Footer Top -->
+        <div class="bottom">
+            <p>&copy 2023 Majulah Munchies. All rights reserved.</p>
+
+        </div>
+    </footer>
 </body>
+
+
 <script src="../asset/js/script.js"></script>
+
+
 </html>
--- html/orderDetails.php
+++ html/orderDetails.php
@@ -44,6 +44,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Just+Another+Hand&family=Koulen&family=Lalezar&family=Mitr:wght@200&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 
@@ -71,55 +74,83 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
 
-
-    </div>
-    <div class="profile-dropdown">
-        <button class="dropbtn">Welcome, <?php echo $_SESSION["user_firstname"] ?>
-            <img src="../asset/image/bingwei.jpeg" alt="Profile Picture" class="profile-pic">
-        </button>
-        <div class="dropdown-content">
-            <a href="profile.php">Profile</a>
-            <a href="logout.php">Log out</a>
-            <a href="#">Orders Details</a>
-        </div>
-    </div>
-    </div>
-
     <div class="order-container">
-        <div>
-            <h2>Order Details</h2>
-            <div class="order-details">
-                <?php
-                foreach ($orderResults as $orderResult) {
-
-                    echo "<div> Order ID: " . $orderResult['OrderID'] . "</div>";
-                    echo "<div> Order Date: " . $orderResult['OrderDate'] . "</div>";
-                    echo "<form action='orderDetails.php' method='post'>";
-                    // Hidden input to store the order ID
-                    echo "<input type='hidden' name='orderID' value='" . $orderResult['OrderID'] . "'>";
-                    // Submit button
-                    echo "<input type='submit' value='View Order'>";
-                    echo "</form>";
-                }
-
-                ?>
-            </div>
-        </div>
-        <div>
+        <div class="order-number-container">
             <div>
-                <?php
-                if ($orderDetailsResults) {
-                    foreach ($orderDetailsResults as $orderDetailResult) {
-                        echo "<div> Item Name: " . $orderDetailResult['FoodName'] . "</div>";
-                        echo "<div> Quantity: " . $orderDetailResult['Quantity'] . "</div>";
-                        echo "<div> Price: " . $orderDetailResult['TotalPrice'] . "</div>";
+                <h2>Order Details</h2>
+                <div class="order-details">
+                    <?php
+                    foreach ($orderResults as $orderResult) {
+                        echo "<div id='wrapper'>";
+                        echo "<div id= 'words'>";
+                        echo "<div> Order ID: " . $orderResult['OrderID'] . "</div>";
+                        echo "<div> Order Date: " . $orderResult['OrderDate'] . "</div>";
+                        echo "<form action='orderDetails.php' method='post'>";
+                        // Hidden input to store the order ID
+                        echo "<input type='hidden' name='orderID' value='" . $orderResult['OrderID'] . "'>";
+                        echo "</div>";
+                        // Submit button
+                        echo "<div id = 'details-submit-button'>";
+                        echo "<input type='submit' value='View Order'>";
+                        echo "</div>";
+                        echo "</form>";
+                        echo "</div>";
                     }
-                }
 
-                ?>
+                    ?>
+                </div>
             </div>
+
+
         </div>
+        <?php
+        $totalPrice = 0;
+        if ($orderDetailsResults) {
+            echo " <div class='order-details-container'>";
+            echo " <div class='product-description'>";
+
+            echo "<table>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th>Description</th>";
+            echo "<th>Quantity</th>";
+            echo "<th>Amount</th>";
+            echo " </tr>";
+            echo "<tbody>"; // Open table body
+
+            foreach ($orderDetailsResults as $orderDetailResult) {
+                echo "<tr>";
+                echo "<td>" . $orderDetailResult['FoodName'] . "</td>";
+                echo "<td>" . $orderDetailResult['Quantity'] . "</td>";
+                echo "<td>" . $orderDetailResult['TotalPrice'] . "</td>";
+                echo "</tr>";
+                $totalPrice += floatval($orderDetailResult['Quantity']) * floatval($orderDetailResult['TotalPrice']);
+            }
+            echo "</tbody>"; // Close table body
+
+            // Add the total price row
+            echo "<tfoot>"; // Open table footer
+            echo "<tr>";
+            echo "<td colspan='2' style='text-align:right;'><strong>Total Price</strong></td>"; // Span two columns for total
+
+            echo "<td class='amount'>" . number_format($orderDetailResult['TotalPrice'], 2) . "</td>";
+            echo "</tr>";
+            echo "</tfoot>"; // Close table footer
+
+            echo "</table>";
+            echo "</div>";
+            echo "</div>";
+        }
+
+        ?>
+
+
     </div>
+
+
+
+
+
 
 </body>
 <script src="../asset/js/script.js"></script>
