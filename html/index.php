2a61ae3e52dca3ff0feba9ff59c5686a40a12c9b
Huaty <siaosiaoboy@gmail.com>
2023-11-13 16:15:18 +0800

    Update

--- html/index.php
+++ html/index.php
@@ -2,17 +2,6 @@
 
 unset($_SESSION['user_firstname']);
 
-if (isset($_SERVER['HTTP_COOKIE'])) {
-  $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
-  foreach ($cookies as $cookie) {
-    $parts = explode('=', $cookie);
-    $name = trim($parts[0]);
-    setcookie($name, '', time() - 1000);
-    setcookie($name, '', time() - 1000, '/');
-  }
-}
-
-
 ?>
 
 <!DOCTYPE html>
